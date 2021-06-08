6784dd20a7d9c64456967083b47ad9eb1f7d1c39
Rahul K <rahukand@gmail.com>
2021-06-08 21:32:45 +0400

    Created UnquantizedNumberPicker.java class which will deal with the UI of the Decimal number picker described in dialog_unquantized.xml

---
+++ app/src/main/java/rkan/project/gow_0b/UnquantizedNumberPicker.java
@@ -0,0 +1,34 @@
+package rkan.project.gow_0b;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class UnquantizedNumberPicker {
+    private LayoutInflater mInflater;
+    private boolean inflated = false;
+    private View rootView;
+    private NumberPicker wholePicker, decimalPicker;
+    UnquantizedNumberPicker(LayoutInflater inflater, String unit) {
+        mInflater = inflater;
+        rootView = inflater.inflate(R.layout.dialog_unquantized, null);
+        ((TextView)rootView.findViewById(R.id.unit_view)).setText(unit);
+        wholePicker = rootView.findViewById(R.id.whole_picker);
+        decimalPicker = rootView.findViewById(R.id.decimal_picker);
+        wholePicker.setMaxValue(99);
+        wholePicker.setMinValue(0);
+        decimalPicker.setMaxValue(99);
+        decimalPicker.setMinValue(0);
+    }
+
+    public double getPickerValue() {
+        rootView.clearFocus();
+        return wholePicker.getValue() + decimalPicker.getValue()*0.01;
+    }
+
+
+}
