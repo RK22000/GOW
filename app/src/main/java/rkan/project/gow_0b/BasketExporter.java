566e35d6fcf6d4e870ea80052c3557c303248be5
Rahul K <rahukand@gmail.com>
2021-06-09 08:53:57 +0400

    BasketExporter now has methods to save and read its basket object to and from a file. 	modified:   app/src/main/java/rkan/project/gow_0b/Basket.java 	modified:   app/src/main/java/rkan/project/gow_0b/BasketExporter.java

--- app/src/main/java/rkan/project/gow_0b/BasketExporter.java
+++ app/src/main/java/rkan/project/gow_0b/BasketExporter.java
@@ -1,8 +1,13 @@
 package rkan.project.gow_0b;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class BasketExporter {
     private Basket mBasket;
@@ -12,15 +17,17 @@ public class BasketExporter {
         //Date
         mOutPutFile = new File(outPutDirectory, "Basket_export_file.txt");
     }
-    public void export() {
+    public boolean export() {
         try {
             FileWriter writer = new FileWriter(mOutPutFile);
             writer.append("Basket list \n")
                     .append(mBasket.toString());
             writer.flush();
             writer.close();
+            return true;
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
     }
     public File getExportedFile() {
@@ -31,7 +38,31 @@ public class BasketExporter {
         }
     }
 
-    public void saveBasketObject() {
+    public boolean saveBasketObject() {
+        try {
+            FileOutputStream fos = new FileOutputStream(mOutPutFile);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(mBasket);
+            oos.close();
+            fos.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
+    public boolean readBasketObject() {
+        try {
+            FileInputStream fis = new FileInputStream(mOutPutFile);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            mBasket = (Basket) ois.readObject();
+            ois.close();
+            fis.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
